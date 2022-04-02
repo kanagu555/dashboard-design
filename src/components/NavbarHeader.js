2d48b85871a02501c8eb94ba0b8e09bac5ec4593
kanagu555 <kanagarajwhb@gmail.com>
2022-04-02 22:33:22 +0530

    navigation bar/header component is done

---
+++ src/components/NavbarHeader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Question, BellFill, PersonFill } from 'react-bootstrap-icons';
+import './NavbarHeader.css'
+import logo from '../assets/logo.PNG'
+
+const NavbarHeader = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} className="logo"/> DIGITAL EAST <span className="company-name">DESSY</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#deets" className="nav-link"><Question className="nav-icon-question" size={25}/><br/>HELP</Nav.Link>
+            <Nav.Link href="#memes" className="nav-link"><BellFill className="nav-icon-bell"/><br/>NOTIFICATIONS</Nav.Link>
+            <Nav.Link href="#memes" className="nav-link"><PersonFill className="nav-icon-person"/><br/>USER</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavbarHeader;
