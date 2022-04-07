cc993456123855c24b3bd7ede80bc88b77ad4ed0
Manoprabha Karunanithi <Manoprabha.K@BRILLIO.COM>
2022-04-07 22:03:17 +0530

    added sample table to the content page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarHeader from "./components/NavbarHeader";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import ContentTable from "./components/ContentTable";
 
 function App() {
   return (
     <div>
         <NavbarHeader />
         <Sidebar />
+        <ContentTable />
         <Footer />
     </div>
   );
